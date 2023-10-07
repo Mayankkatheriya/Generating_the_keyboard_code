fd1434bec1338bbf7a19dc0465d3a47c52a33dc6
Mayankkatheriya <mayankkatheriya4@gmail.com>
2023-10-08 01:41:56 +0530

    ctrl and shift key Added

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 let keyBox = document.querySelector(".keyBox");
 const audio = new Audio("./mixkit-hard-single-key-press-in-a-laptop-2542.wav");
 document.body.addEventListener("keydown", function(e){
-    console.log(e);
     let key = e.key;
+    if(e.shiftKey && e.keyCode!=16){
+        key = `Shift+${e.key}`
+    }
+    if(e.ctrlKey&& e.keyCode!=17){
+        key = `Ctrl+${e.key}`
+    }
     let keyCode = e.keyCode;
-    console.log(keyCode);
     keyBox.innerHTML = `
     <h1 class = "key">You Pressed: <span>${key}</span></h1>
     <div class = "code">${keyCode}</div>`;
